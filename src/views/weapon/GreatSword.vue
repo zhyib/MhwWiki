64908b7ab5196baaebee003efd2bdea57619457b
zhyib <zhyib002@gmail.com>
2020-02-19 21:05:54 -0500

    武器部分模块化

--- src/views/weapon/GreatSword.vue
+++ src/views/weapon/GreatSword.vue
@@ -1,56 +1,14 @@
 <template>
-  <div>
-    <el-table
-      :data="tableData"
-      row-key="id"
-      border
-      default-expand-all
-      :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
-      <el-table-column
-        prop="name"
-        label="武器名"
-      />
-      <el-table-column
-        prop="attack"
-        label="攻击"
-        sortable
-      />
-      <el-table-column
-        prop="affinity"
-        label="会心"
-        sortable
-      />
-      <el-table-column
-        prop="element"
-        label="属性"
-      >
-        <template v-slot:default="scope">
-          {{ scope.row.element }} {{ scope.row.elementVal }}
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop="sharpness"
-        label="斩味"
-      >
-        <template slot-scope="scope">
-          <sharpness :content="scope.row.sharpness" :full="scope.row.fullSharp"></sharpness>
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop="slot"
-        label="孔位"
-      />
-    </el-table>
-  </div>
+  <melee-weapon :data="tableData"></melee-weapon>
 </template>
 
 <script>
-import Sharpness from '@/components/Sharpness/index'
+import MeleeWeapon from '@/components/MeleeWeapon/index'
 
 export default {
   name: 'GreatSword',
   components: {
-    'sharpness': Sharpness
+    'melee-weapon': MeleeWeapon
   },
   data() {
     return {
