346b593ebf09f94d5ae3676874e0623c11ecec44
zhyib <zhyib002@gmail.com>
2020-02-21 21:09:12 -0500

    fix config

--- vue.config.js
+++ vue.config.js
@@ -25,8 +25,8 @@ module.exports = {
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/MhwWiki/'
-    : '/',
+    ? './'
+    : './',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
