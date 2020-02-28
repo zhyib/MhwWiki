f26f05fadd11c0a182530842e79a98259d61d3f5
zhyib <zhyib002@gmail.com>
2020-02-28 16:25:22 -0500

    更新dashboard

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,24 +1,53 @@
 <template>
   <div class="dashboard-container">
-    <div>
-      正在搞的内容
-    </div>
-    <el-row>
-      <el-col :span="3">
-        武器
-      </el-col>
-      <el-col :span="3">
-        <el-progress :percentage="10"></el-progress>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="3">
-        怪物
-      </el-col>
-      <el-col :span="3">
-        <el-progress :percentage="5"></el-progress>
-      </el-col>
-    </el-row>
+    <h1>进度一览</h1>
+    <el-card class="box-card">
+      <div slot="header">
+        <span>样式</span>
+      </div>
+      <div v-for="(item, index) in styleCard" :key="index">
+        <el-row>
+          <el-col :span="6">
+            {{ item.name }}
+          </el-col>
+          <el-col :span="18">
+            <el-progress :percentage="item.progress" />
+          </el-col>
+        </el-row>
+      </div>
+    </el-card>
+    <br>
+    <el-card class="box-card">
+      <div slot="header">
+        <span>数据</span>
+      </div>
+      <div v-for="(item, index) in dataCard" :key="index">
+        <el-row>
+          <el-col :span="6">
+            {{ item.name }}
+          </el-col>
+          <el-col :span="18">
+            <el-progress :percentage="item.progress" />
+          </el-col>
+        </el-row>
+      </div>
+    </el-card>
+    <br>
+    <el-card class="box-card">
+      <div slot="header">
+        <span>样式</span>
+      </div>
+      <div v-for="(item, index) in totalCard" :key="index">
+      <el-row>
+          <el-col :span="6">
+            {{ item.name }}
+          </el-col>
+          <el-col :span="18">
+            <el-progress :percentage="item.progress" />
+          </el-col>
+        </el-row>
+      </div>
+    </el-card>
   </div>
 </template>
 
@@ -27,6 +56,32 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'Dashboard',
+  data() {
+    return {
+      activeName: 'first',
+      styleCard: [{
+        name: '武器',
+        progress: 80
+      }, {
+        name: '怪物',
+        progress: 50
+      }],
+      dataCard: [{
+        name: '武器',
+        progress: 5
+      }, {
+        name: '怪物',
+        progress: 3
+      }],
+      totalCard: [{
+        name: '路由调整',
+        progress: 10
+      }, {
+        name: '删除多余功能',
+        progress: 0
+      }]
+    }
+  },
   computed: {
     ...mapGetters([
       'name'
@@ -40,10 +95,18 @@ export default {
     &-container {
       margin: 30px;
     }
-
     &-text {
       font-size: 30px;
       line-height: 46px;
     }
+  }
+  .text {
+    font-size: 14px;
+  }
+  .item {
+    margin-bottom: 18px;
+  }
+  .box-card {
+    width: 480px;
   }
 </style>
